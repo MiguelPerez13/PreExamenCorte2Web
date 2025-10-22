5aee91e32f5766d63b968b03eae8f802678de277
Miguel <perezcontrerasmiguelalejandro@gmail.com>
2025-10-22 09:18:43 -0700

    se arrego json de practica 1

--- js/practica01.js
+++ js/practica01.js
@@ -1,15 +1,15 @@
-import { presion } from "../js/presion.js";
-
-
 const tablebody = document.getElementById('tableBody');
 const promedio = document.getElementById('promedio');
 
 let presionTotal = 0;
 let dias = 0;
 
-const inicializar = ()=> {
+const inicializar = async()=> {
 
-    presion.forEach( dato =>{
+    await fetch('../js/presion.json')
+    .then(respuesta => respuesta.json())
+    .then( datos => {
+        datos.forEach( dato =>{
         const fila = document.createElement('tr');
         
         const celdaNombre = document.createElement('td');
@@ -24,15 +24,18 @@ const inicializar = ()=> {
         celdaPresion.innerText = dato.presion_arteria;
         fila.appendChild(celdaPresion);
         
-        presionTotal += dato.presion_arteria;
+        presionTotal += parseFloat(dato.presion_arteria);
+        
         dias++;
+        
         
         tablebody.appendChild(fila);
     });
-
+    })
+    
     let presionPromedio = presionTotal / dias;
-
-    promedio.innerText = `Su promedio arterial es ${presionPromedio}`;
+    promedio.innerText = `Promedio de presion arterial ${presionPromedio}`;
+    
 }
     
     
